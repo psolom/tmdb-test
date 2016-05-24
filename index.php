84beb51d2a34ae0319018357fedf467cf2d07688
pavel <solomienko1802@gmail.com>
2016-05-24 12:45:39 +0300

    remove debug mode

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $autoload = dirname(__FILE__) . '/protected/vendor/autoload.php';
 $config = dirname(__FILE__) . '/protected/config/main.php';
 
 // remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_DEBUG') or define('YII_DEBUG', false);
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
 
